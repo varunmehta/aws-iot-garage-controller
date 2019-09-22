49ba1a96ea05976d170da98b0babd6e1a602f809
Varun Mehta <varun.mehta@S00405-MBPR.local>
2019-09-21 23:15:09 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# AWS IoT endpoint settings
+HOST_NAME = "your-aws-account-specific-prefix.iot.your-region.amazonaws.com"
+HOST_PORT = 8883
+
+# Thing certs & keys
+PRIVATE_KEY = "certs/private.pem.key"
+DEVICE_CERT = "certs/certificate.pem.crt"
+ROOT_CERT = "certs/root-CA.crt"
+
+# Message settings
+SENSOR_TOPIC = "garagedoor"
+QOS_LEVEL = 0
+
+
+# GPIO PIN CONFIG
+# In the future, fetch this from cloud config, so you can change as needed, without having to change code.
+MOTOR_OPEN = 4
+MOTOR_CLOSE = 14
+DOOR_SENSOR = 24
---
+++ main.py
@@ -0,0 +1,95 @@
+"""
+This is the main garage controller. The features it entails.
+ - Open/Close garage on MQTT commands
+ - Log status of magnetic switch to garage (this is polling every 10 seconds, we could bring it down to 1 too)
+  -- Once the status is detected to be "Open", if more than 2 minutes, send an SNS notification of the garage
+     being open for too long.
+ - Camera starts recording the moment the switch status is changed to Open
+    -- Add motion pi if possible, but too much config to handle.
+    -- Can camera always be in preview mode ? and only save to disk when it detects motion ??
+ - Alexa ask CarPen to open
+ - Alexa ask CarPen to close
+ - Alexa ask CarPen the status ~ Return Open or Close
+"""
+import config
+import logging
+from gpiozero import Motor
+from gpiozero import Button
+from datetime import datetime
+# from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+# forward is considering closing the garage
+# backward is considered opening the garage
+motor = Motor(forward=config.MOTOR_CLOSE, backward=config.MOTOR_OPEN)
+sensor = Button(config.DOOR_SENSOR, pull_up=True, hold_time=150)
+# allowed_actions = ['both', 'publish', 'subscribe']
+
+'''
+    When the door is open for too long, you can send an SNS alert.
+'''
+
+
+def open_too_long_alert():
+    print("Door open for too long. Do you want to close it ? ")
+    return
+
+
+'''
+    Status of the door is open or closed.  
+'''
+
+
+def send_message(status):
+    print("Door status changed " + status)
+
+
+def garage_opened():
+    send_message("Opened")
+    return
+
+
+def garage_closed():
+    send_message("Closed")
+    return
+
+
+"""
+    Meat of the logic to handle garage door open-close.
+    Verify against sensor to check if the garage is already open or closed.
+"""
+
+
+def open_close_garage(action):
+    sensor.when_activated = garage_opened
+
+    sensor.when_held = open_too_long_alert
+
+    if sensor.is_pressed:
+        motor.forward()
+
+    motor.stop()
+
+    return
+
+# Configure logging
+# logger = logging.getLogger("AWSIoTPythonSDK.core")
+# logger.setLevel(logging.INFO)
+# streamHandler = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# streamHandler.setFormatter(formatter)
+# logger.addHandler(streamHandler)
+
+# Initialize MQTT client
+# client = AWSIoTMQTTClient('')
+
+# Configure client endpoint, port information, certs
+# client.configureEndpoint(config.HOST_NAME, config.HOST_PORT)
+# client.configureCredentials(config.ROOT_CERT, config.PRIVATE_KEY, config.DEVICE_CERT)
+# client.configureOfflinePublishQueueing(-1)
+# client.configureDrainingFrequency(2)
+# client.configureConnectDisconnectTimeout(10)
+# client.configureMQTTOperationTimeout(5)
+
+# Connect
+print('Connecting to endpoint ' + config.HOST_NAME)
+# client.connect()
